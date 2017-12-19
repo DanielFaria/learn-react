10b953c4ca7de0a55dae7886271f91d80e174edf
Daniel Faria <lenin@stack.com>
2017-12-19 15:08:40 -0200

    Now We had a stateless component - no class-no render method.I learn with  this: hackernoon.com/react-stateless-functional-components-nine-wins-you-might-have-overlooked-997b0d933dbc

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import Count from './components/Count'
+import Hello from './components/StateLessHello.js'
 import registerServiceWorker from './registerServiceWorker';
+//I can encapsulate a cointainer component like  that?
+//Eu não sabia. 
+let app = <div> <Hello name={"React !!!"}/> <Count /> </div>;
 
-ReactDOM.render(<Count />, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
